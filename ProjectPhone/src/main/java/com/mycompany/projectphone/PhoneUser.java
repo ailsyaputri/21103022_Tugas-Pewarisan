8375e56f73338bb470b3380d2e1ac92aada2caf4
Ailsya Putri <ailsyaputri9597@gmail.com>
2022-11-18 20:39:19 +0700

    Komentar

---
+++ ProjectPhone/src/main/java/com/mycompany/projectphone/PhoneUser.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projectphone;
+
+/**
+ *
+ * @author ASUS
+ * 21103022_Ailsya Putri_SI05A
+ */
+public class PhoneUser {
+    private Phone phone;
+    public PhoneUser(Phone phone){
+        this.phone = phone;
+    }
+    void turnOnThePhone(){
+        this.phone.powerOn();
+    }
+    void turnOffThePhone(){
+        this.phone.powerOff();
+    }
+    void makePhoneLouder(){
+        this.phone.volumeUp();
+    }
+    void makePhoneSilent(){
+        this.phone.volumeDown();
+    }
+}
